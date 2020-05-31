f5f6d94abba2eb0a9370b27efa3d6af136a8d475
kemot1709 <37084908+kemot1709@users.noreply.github.com>
2020-05-31 04:36:06 +0200

    Start of project 3

---
+++ proj_3/params.m
@@ -0,0 +1,12 @@
+% Seed
+numer_albumu = 293457;
+rng(numer_albumu);
+
+% Generate data
+N=20;
+A=[randn(N/2,1) rand(N/2,1)+0.5; randn(N/2,1) -rand(N/2,1)-0.5];
+angle=randn(1); 
+x(:,1)=A(:,1)*cos(angle)-A(:,2)*sin(angle); 
+x(:,2)=A(:,1)*sin(angle)+A(:,2)*cos(angle); 
+x=x+randn(1,2); 
+y=sign(A(:,2));
